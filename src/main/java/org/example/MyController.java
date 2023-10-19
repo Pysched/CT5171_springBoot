e9c6fa5e6cc24e5c08a2f9601ce1e3e91ded4509
Pysched <48335713+Pysched@users.noreply.github.com>
2023-10-19 19:10:32 +0100

    Initial commit

---
+++ src/main/java/org/example/MyController.java
@@ -0,0 +1,25 @@
+package	org.example;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.format.DateTimeFormat;
+import	org.springframework.web.bind.annotation.GetMapping;
+import	org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public	class	MyController	{
+    @GetMapping("/")
+    @ResponseBody
+    public	String	index()	{
+       DateTime time = DateTime.now();
+       DateTime xmas = DateTime.parse("25/12/2023", DateTimeFormat.forPattern("dd/MM/yyyy"));
+       int days = Days.daysBetween(time, xmas).getDays();
+       return "days left to christmas " + days;
+    }
+    @RequestMapping ("/home")
+        public String home(){
+            return " index.html";
+
+    }
+}
